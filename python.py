4805ff6bc395e0b89e7ce14c6418e2426453eafe
Jeewan Rai <jeewanrai@dhi.bt>
2023-07-11 07:08:18 +0600

    changed code, discussion on decorator

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
-print("jeewan is creat")
+def f1(func):
+    def wrapper():
+        func()
+        print("Started")
+        print("Ended")
+        func()
+    return wrapper
+
+@f1
+def f():
+    print("Hello")
+f()
+
